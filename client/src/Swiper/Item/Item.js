8a8fe503b26626ca4acba072255771ace401709e
Michael <mkeating2225@gmail.com>
2017-10-21 14:10:09 -0400

    setting a breakpoint before restructuring to allow for swipable interactions

--- client/src/Swiper/Item/Item.js
+++ client/src/Swiper/Item/Item.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 
 
 class Item extends Component {
   render( ) {
     return (
-      <div className="item" id={this.props.id}>
-        {this.props.content}
-      </div>
+
+    	<Draggable
+    		axis="x"
+
+
+    	>
+    		<span>
+		      <div className="item" id={this.props.id}>
+		        {this.props.content}
+		      </div>
+		    </span>
+	    </Draggable>
     );
   }
 }
